574f4257c62f116d0219baa9ee9e7b7060cd1731
Josh Wilcox <joshuawilcox63@gmail.com>
2024-07-03 14:10:07 -0500

    Create Computer choice logic
    Created the part where we randomly generated a choice for the computer

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!')
+function getComputerChoice(){
+  let randomNumber = Math.random() * 100 + 1;
+  if (randomNumber <= 33) {
+    return 'Rock'
+  }
+  else if (randomNumber > 33 && randomNumber <= 66){
+    return 'Paper'
+  }
+  else {
+    return 'Scissors'
+  }
+}
+
+
+console.log(getComputerChoice())
